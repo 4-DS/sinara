09e950cbb64802837bfbabef329b92e41371957d
Maxim Buslovaev <maksim.buslovaev@outlook.com>
2023-03-10 21:07:10 +0300

    Added bentoml.py

---
+++ bentoml.py
@@ -0,0 +1,46 @@
+from ..env import get_curr_run_id, get_dsml_component_tmp_path
+from ..fs import FileSystem
+import os
+import shutil
+from pathlib import Path
+import logging
+import time
+import requests
+import bentoml
+from enum import Enum
+import json
+import yaml
+from subprocess import STDOUT, PIPE, run, Popen
+from .utils import *
+
+
+
+def save_bentoservice( bentoservice, fspath ):
+    offline_build=True
+    ''' save to fs model packed as a BentoService Python object '''
+
+    #write bento service to tmp dir
+    runid = get_curr_run_id()
+    
+    bentoservice_name = os.path.basename(fspath)
+    tmppath = get_dsml_component_tmp_path()
+    bentoservice_dir = f"{tmppath}/{runid}/{bentoservice_name}"
+    
+    shutil.rmtree(bentoservice_dir, ignore_errors=True)
+    os.makedirs(bentoservice_dir, exist_ok=True)
+    
+    bentoservice.save_to_dir(bentoservice_dir)
+    
+    #make zip file for bento service
+    bentoservice_zipfile =  f"{tmppath}/{runid}_{bentoservice_name}.model" 
+    shutil.make_archive(bentoservice_zipfile, 'zip', bentoservice_dir)
+
+    #write zip file to fs
+    
+    fs = FileSystem.FileSystem()
+    fs.makedirs(fspath)
+    fs.put(f"{bentoservice_zipfile}.zip", f"{fspath}/model.zip")
+    fs.touch(f"{fspath}/_SUCCESS")
+    
+#remove zip file from tmp
+    os.remove(f"{bentoservice_zipfile}.zip")
