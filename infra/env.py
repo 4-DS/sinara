8deb26d65f1e2ac6dafe1e5a65046ac5ef1b0500
Ilya Pestov <ilya.pestov@outlook.com>
2023-10-09 22:01:59 +0300

    Changed infra and env management

---
+++ infra/env.py
@@ -0,0 +1,42 @@
+import json
+import os
+
+def get_cache_paths():
+    cache_paths = {
+        "test": "/data/tmp/test",
+        "prod": "/data/tmp/prod",
+        "user": "/data/tmp/user"
+    }
+    return cache_paths
+
+def get_cache_path(env_name):
+    
+    env_paths = get_cache_paths()
+    if env_name not in env_paths:
+        raise Exception("Unexpected env_name value:" + env_name)
+
+    return env_paths[env_name]
+
+def get_user():
+    return os.getenv("DSML_USER") or 'jovyan'
+
+def get_data_paths():
+    data_paths = {
+        "test": "/data/products",
+        "prod": "/data/production",
+        "user": f"/data/home/{get_user()}"
+    }
+    
+    custom_data_paths = {}
+    if os.path.isfile("sinara/infra/env.json"):
+        with open("sinara/infra/env.json") as json_file:
+            custom_data_paths = json.load(json_file)
+
+        data_paths = {**data_paths,**custom_data_paths}
+    return data_paths
+
+def get_env_path(env_name):
+    env_paths = get_data_paths()
+    if env_name not in env_paths:
+        raise Exception("Unexpected env_name value:" + env_name)
+    return env_paths[env_name]
