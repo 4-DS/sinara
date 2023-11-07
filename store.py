9da01ba75ab6dafc14944486ef8c0a1553943feb
pestovid <ilya.pestov@outlook.com>
2023-11-07 13:27:03 +0000

    fixed SinaraStore

--- store.py
+++ store.py
@@ -19,11 +19,11 @@ class SinaraStore:
         
     @staticmethod
     def copy_tmp_files_to_store(tmp_dir=str, store_dir=str, file_globs=["*"]):
-        archive_tmp_files_to_store(tmp_dir, store_dir, file_globs)
+        SinaraStore.archive_tmp_files_to_store(tmp_dir, store_dir, file_globs)
 
     @staticmethod
     def copy_store_files_to_tmp(store_dir=str, tmp_dir=str, file_globs=["*"]):
-        dearchive_store_files_to_tmp(store_dir, tmp_dir, file_globs)
+        SinaraStore.dearchive_store_files_to_tmp(store_dir, tmp_dir, file_globs)
         
     @staticmethod
     def archive_tmp_files_to_store(tmp_dir=str, store_path=str, file_globs=["*"]):
@@ -41,18 +41,18 @@ class SinaraStore:
             raise Exception("file_globs doesn't match any file")
         
         fs = SinaraFileSystem.FileSystem()
-        fs.makedirs(store_dir)
+        fs.makedirs(store_path)
         # for tmp_file_path in filenames:
-        #     store_file_path = str(Path(store_dir, Path(tmp_file_path).name))
+        #     store_file_path = str(Path(store_path, Path(tmp_file_path).name))
         #     fs.put(tmp_file_path, store_file_path)
         tar_file_path = f'{tmp_dir}/files.tar'
         with tarfile.open(tar_file_path, 'w') as tar:
             for tmp_file_path in filenames:
                 tar.add(tmp_file_path, arcname=tmp_file_path.replace(tmp_dir, ''))
 
-        store_file_path = str(Path(store_dir, Path(tar_file_path).name))
+        store_file_path = str(Path(store_path, Path(tar_file_path).name))
         fs.put(tar_file_path, store_file_path)
-        fs.touch(Path(store_dir, '_SUCCESS'))
+        fs.touch(Path(store_path, '_SUCCESS'))
         Path(tar_file_path).unlink()
         
         
@@ -69,17 +69,17 @@ class SinaraStore:
         
         filenames = []
         for gl in file_globs:
-            filenames += fs.glob(str(Path(store_dir, gl)))
+            filenames += fs.glob(str(Path(store_path, gl)))
         if len(filenames) == 0:
             raise Exception("file_globs doesn't match any file")
         
-        # store_globs = fs.glob(store_dir)
+        # store_globs = fs.glob(store_path)
         # makedirs(tmp_dir, exist_ok=True)
         fs.makedirs(tmp_dir)
         # for store_file_path in filenames:
         #     tmp_file_path = str(Path(tmp_dir, Path(store_file_path).name))
         #     fs.get(store_file_path, tmp_file_path)
-        store_file_path = str(Path(store_dir, 'files.tar'))
+        store_file_path = str(Path(store_path, 'files.tar'))
         tar_file_path = str(Path(tmp_dir, Path(store_file_path).name))
         fs.get(store_file_path, tar_file_path)
         with tarfile.open(tar_file_path) as tar:
