494e1bdff0a20ce8d9e2496eb67b96e3a4526c83
buslovaev_ma <buslovaev_ma@outlook.com>
2023-12-19 12:54:04 +0000

    onnx fixed imports

--- bentoml_artifacts/__init__.py
+++ bentoml_artifacts/__init__.py
@@ -1,3 +1,2 @@
 from .bento_archive import *
 from .binary_artifact import *
-from .onnx_artifact import *
---
+++ bentoservice/__init__.py
@@ -0,0 +1,3 @@
+from .binary_artifact import *
+from .onnx_artifact import *
+from .profiles import *
