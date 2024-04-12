35466f3a74bc72b13f534f1e5dba074743901b2a
pestovid <ilya.pestov@outlook.com>
2024-04-12 14:44:14 +0000

    fixed parameter name

--- archive.py
+++ archive.py
@@ -6,13 +6,13 @@ from functools import partial
 from pyspark.sql.functions import regexp_replace,col,lit
 from urllib.parse import urlsplit
  
-def SinaraArchive_save_file(file_col, tmp_dir):
+def SinaraArchive_save_file(file_col, tmp_entity_dir):
     '''
     _save_file defined as function since runtime error when declared as method:
     It appears that you are attempting to reference SparkContext from a broadcast variable, action, or transformation.
     SparkContext can only be used on the driver, not in code that it run on workers. For more information, see SPARK-5063.
     '''
-    file_name = path.join(tmp_dir, file_col.relPath.strip('/'))
+    file_name = path.join(tmp_entity_dir, file_col.relPath.strip('/'))
     file_binary = file_col.content
 
     os.makedirs(path.dirname(file_name), exist_ok=True)
@@ -27,13 +27,13 @@ class SinaraArchive:
     def __init__(self, spark):
         self._spark = spark;
         
-    def pack_files_form_tmp_to_spark_df(self, tmp_dir):
-        tmp_url = tmp_dir
-        url = urlsplit(tmp_dir)
+    def pack_files_form_tmp_to_spark_df(self, tmp_entity_dir):
+        tmp_url = tmp_entity_dir
+        url = urlsplit(tmp_entity_dir)
         if not url.scheme:
             tmp_url = f'file://{url.path}'
 
-        pathlist = [x for x in Path(tmp_dir).glob(f'**/*') if not str(x.name).endswith(".parts") and not str(x.parent).endswith(".parts")]
+        pathlist = [x for x in Path(tmp_entity_dir).glob(f'**/*') if not str(x.name).endswith(".parts") and not str(x.parent).endswith(".parts")]
         total_size = 0
         for path in pathlist:
             if Path(path).is_file():
@@ -47,21 +47,21 @@ class SinaraArchive:
         df = self._spark.read.format("binaryFile").option("pathGlobFilter", "*").option("recursiveFileLookup", "true") \
                 .load(tmp_url) \
                 .filter(col('length') <= self.ROW_SIZE) \
-                .withColumn("relPath", regexp_replace('path', 'file:' + tmp_dir, '')) \
+                .withColumn("relPath", regexp_replace('path', 'file:' + tmp_entity_dir, '')) \
                 .drop("path")
         return df.repartition(partitions)
     
-    def pack_files_from_tmp_to_store(self, tmp_dir, store_path):
-        df = self.pack_files_form_tmp_to_spark_df(tmp_dir)
+    def pack_files_from_tmp_to_store(self, tmp_entity_dir, store_path):
+        df = self.pack_files_form_tmp_to_spark_df(tmp_entity_dir)
         df.write.option("parquet.block.size", self.BLOCK_SIZE).mode("overwrite").parquet(store_path)
     
-    def unpack_files_from_spark_df_to_tmp(self, df_archive, tmp_dir):
-        df_archive.foreach(partial(SinaraArchive_save_file, tmp_dir=tmp_dir))
-        self._join_parts(tmp_dir)
+    def unpack_files_from_spark_df_to_tmp(self, df_archive, tmp_entity_dir):
+        df_archive.foreach(partial(SinaraArchive_save_file, tmp_entity_dir=tmp_entity_dir))
+        self._join_parts(tmp_entity_dir)
     
-    def unpack_files_from_store_to_tmp(self, store_path, tmp_dir):
+    def unpack_files_from_store_to_tmp(self, store_path, tmp_entity_dir):
         df = self._spark.read.parquet(store_path)
-        self.unpack_files_from_spark_df_to_tmp(df, tmp_dir)
+        self.unpack_files_from_spark_df_to_tmp(df, tmp_entity_dir)
         
     def _split_file(self, path, chunk_size):
         parts_path = Path(f"{path.parent}/{path.name}.parts")
