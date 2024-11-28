78c5fe951dbbf6c85ea3998a8e3098b4ba9f409d
Ilya Pestov <ilya.pestov@outlook.com>
2024-11-28 21:34:56 +0300

    check infra

--- common.py
+++ common.py
@@ -4,9 +4,12 @@ import os
 def importSinaraModuleClass(module_name, class_name):
     #module = importlib.import_module('sinara.infra.local_filesystem.spark')
     if "INFRA_NAME" in os.environ:
-        module_package = f"sinara.infra.{os.environ['INFRA_NAME']}.{module_name}"
-        #print (module_package)
-        module = importlib.import_module(module_package)
+        try:
+            module_package = f"sinara.infra.{os.environ['INFRA_NAME']}.{module_name}"
+            #print (module_package)
+            module = importlib.import_module(module_package)
+        except ModuleNotFoundError:
+            raise Exception(f"Sinara lib, doesn't configured for infra '{os.environ['INFRA_NAME']}'. Please use correct infrastructure.")
         module_class = getattr(module, class_name)
         
     else:
